4efe808ef100fbff31d1b9ddc4dc9a15a6f6abe1
David <Muchahi 853@gmail.com>
2024-03-29 01:27:21 +0300

    Deleted repo submission

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(){
+    const num1=100;
+    const num2=2000;
+    return num2/num1;
+    
+    
+
+}
+console.log(divide());
+
+const square= num3=>num3*num3;
+const add= (num4,num5)=>num4+num5;
+console.log(square(4));
